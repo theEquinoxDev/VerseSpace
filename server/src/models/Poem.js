e591585c66536dc67ff0cd8c66460fc5ce1edd07
Aditya <adityasuri11ss@gmail.com>
2025-10-29 22:09:26 +0530

    Initial commit

---
+++ server/src/models/Poem.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const poemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Poem = mongoose.model("Poem", poemSchema);
+
+export default Poem;
