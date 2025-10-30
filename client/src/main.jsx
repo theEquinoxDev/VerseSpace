72103ca2286ef7ff0d3d6fbbe8a47a386e91a9f8
Aditya <adityasuri11ss@gmail.com>
2025-10-31 03:15:13 +0530

    added main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import useUserStore from './store/userStore';
+
+useUserStore.getState().initializeAuth();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
