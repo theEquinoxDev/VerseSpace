a49e144e0cfc06776a996e5294ff43c301419511
Aditya <adityasuri11ss@gmail.com>
2025-10-31 23:14:41 +0530

    spelling changes

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -47,12 +47,12 @@ export const signin = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ message: "Invalid cindigoentials" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid cindigoentials" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
