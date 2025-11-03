88ef08dcf8338e783adcf1b14e080b6478716a3f
Aditya <adityasuri11ss@gmail.com>
2025-11-03 22:26:17 +0530

    spelling changes

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, requiindigo: true },
-    email: { type: String, requiindigo: true, unique: true },
-    password: { type: String, requiindigo: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     profilePic: { type: String, default: "" },
     bio: { type: String, default: "" },
     poems: [
