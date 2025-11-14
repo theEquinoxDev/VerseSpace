65e9492ffc8424067350f9e8137170b223d869d0
Aditya <adityasuri11ss@gmail.com>
2025-11-14 14:21:06 +0530

    fixed cors issue for local testing

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'https://versespace.vercel.app' }));
+app.use(cors({ origin: ['https://versespace.vercel.app', 'http://localhost:5173'] }));
 
 connectDB();
 
